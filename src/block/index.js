9ebafc8e2cd3d15caf8de790e5e0e1fd84f20e87
James Allardice <james@allardice.me>
2018-09-25 12:15:03 +0100

    Add a "createHash" method to the Block class

--- src/block/index.js
+++ src/block/index.js
@@ -1,7 +1,17 @@
+import crypto from 'crypto';
+
 export default class Block {
   constructor(index, data) {
     this.index = index;
     this.data = JSON.stringify(data);
     this.timestamp = new Date().getTime();
+    this.hash = this.createHash();
+  }
+
+  createHash() {
+    return crypto
+      .createHash('sha256')
+      .update(this.index + this.data + this.timestamp)
+      .digest('hex');
   }
 }
