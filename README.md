01b934fcd4cb2c12cf6d79b878736c665075b1ee
James Allardice <james@allardice.me>
2018-09-25 12:10:59 +0100

    Boilerplate for HTTP and P2P server

---
+++ README.md
@@ -0,0 +1,15 @@
+# jellycoin
+
+A naive blockchain implementation in JavaScript for teaching purposes. It is not
+recommended that you use any of the code in this project for production
+purposes.
+
+## Usage
+
+There are a number of tags in the repository marking various stages of
+development. If you are attending the "Do you want to build a blockchain"
+workshop or conference session you can use these tags to jump ahead to the next
+section if you need to.
+
+To get started, clone the repository, run `npm install` in the root directory
+and then `npm start` to start the HTTP server and P2P connection.
---
+++ src/node/index.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import Swarm from 'discovery-swarm';
+import getPort from 'get-port';
+import bodyParser from 'body-parser';
+
+const P2P_CHANNEL = 'jc';
+const server = express();
+const swarm = Swarm();
+const peers = [];
+
+// Utility functions for P2P communication. We can "broadcast" a message to the
+// entire network or we can directly "message" an individual peer.
+function broadcast(message) {
+  peers.forEach(peer => peer.write(JSON.stringify(message)));
+}
+
+function message(peer, message) {
+  peer.write(JSON.stringify(message));
+}
+
+// Start the HTTP server and the P2P connection.
+(async () => {
+  const HTTP_SERVER_PORT = await getPort();
+  const P2P_SERVER_PORT = await getPort();
+
+  // Start the P2P server and join the control channel for this blockchain. When
+  // we receive connections from peers we set up a number of handlers to respond
+  // to messages received from that peer.
+  swarm.listen(P2P_SERVER_PORT);
+  swarm.join(P2P_CHANNEL);
+
+  swarm.on('connection', (connection) => {
+    peers.push(connection);
+
+    connection.on('data', (data) => {
+      const obj = JSON.parse(data);
+
+      switch (obj.message) {
+        default:
+          // Ignore unknown messages.
+      }
+    });
+  });
+
+  // Start an HTTP server on a given port. Nodes in our network offer a limited
+  // HTTP API through which they can be controlled.
+  server.use(bodyParser.json());
+  server.listen(HTTP_SERVER_PORT, () => {
+    console.log(`\nHTTP server listening on port ${HTTP_SERVER_PORT}`);
+  });
+})();
