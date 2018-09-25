f9f4dd9b2be0546ff5df76afcb5c1d541516e5e5
James Allardice <james@allardice.me>
2018-09-25 12:16:11 +0100

    Include the hash of the previous block in the new block hash

--- src/block/index.js
+++ src/block/index.js
@@ -1,17 +1,18 @@
 import crypto from 'crypto';
 
 export default class Block {
-  constructor(index, data) {
+  constructor(index, data, previousHash) {
     this.index = index;
     this.data = JSON.stringify(data);
     this.timestamp = new Date().getTime();
     this.hash = this.createHash();
+    this.previousHash = previousHash;
   }
 
   createHash() {
     return crypto
       .createHash('sha256')
-      .update(this.index + this.data + this.timestamp)
+      .update(this.index + this.data + this.timestamp + this.previousHash)
       .digest('hex');
   }
 }
